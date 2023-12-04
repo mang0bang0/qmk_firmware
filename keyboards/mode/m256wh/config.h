aeb907f0f4be1c627260bffc411de634c7c34f65
mango <mangogit@protonmail.com>
2023-12-03 20:34:21 -0500

    envoy: define custom map and custom capslock

--- keyboards/mode/m256wh/config.h
+++ keyboards/mode/m256wh/config.h
@@ -24,4 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define WS2812_DMA_STREAM STM32_DMA2_STREAM5
 #define WS2812_DMA_CHANNEL 6
 
+#define TAPPING_TERM 125
+
 #define EECONFIG_KB_DATA_SIZE (1)
