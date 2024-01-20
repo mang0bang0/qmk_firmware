e76132cbe2e9658fc04e8a32d5e2a3bee1fa041b
mango <mangogit@protonmail.com>
2024-01-20 17:34:01 -0500

    tempo add dedicated esc key under whitespace

--- keyboards/mode/m60h_f/keymaps/mango/keymap.c
+++ keyboards/mode/m60h_f/keymaps/mango/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI
     ),
     [1] = LAYOUT(
-        KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         QK_BOOT, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          RGB_TOG,
         KC_TRNS,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS, _______,
