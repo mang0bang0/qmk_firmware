b32ed111db397001f4a7c3fb85acc56c16eb86b1
mango <mangogit@protonmail.com>
2024-07-03 21:43:38 -0400

    neo ergo update for gaming

--- keyboards/neo/ergo/keymaps/mango/keymap.c
+++ keyboards/neo/ergo/keymaps/mango/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_ESC,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_ESC,                    KC_TRNS, KC_SPC,  KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [2] = LAYOUT_hot(
